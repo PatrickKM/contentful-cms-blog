3aa6d483bc73b7a7835967b17f7e2b92484031e5
PatrickKM <32448400+PatrickKM@users.noreply.github.com>
2020-05-06 14:21:12 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from 'gatsby';
+import Nav from '../components/nav';
+import Featured from '../components/featured';
+import Home from '../components/home';
+import Footer from '../components/footer';
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import './index.css';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <Nav />
+    <Featured />
+    <Home />
+    <Link to='/blog' className='viewmore'>View More</Link>
+    <Footer />
+  </Layout>
+)
+
+export default IndexPage
